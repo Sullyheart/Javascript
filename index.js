be8dd9fda6793e17fa97795d5a7ce931079331d2
Suliyat <sullieart@gmail.com>
2020-03-26 10:44:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstName = 'Suliyat'
+let middleName = 'Opeyemi'
+let lastName = 'Abdulsalam'
+console.log(firstName, middleName, lastName);
+
+let selectedCourses = ['HTML', 'CSS', 'JAVASCRIPT'];
+console.log(selectedCourses);
+
+for (let i = 1; i<200; i++){
+    if (i % 2 !==0)
+    console.log(i);
+}
